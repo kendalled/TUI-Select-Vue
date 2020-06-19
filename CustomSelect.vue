04af08520cf2857b6c3a507cd1bae66fee8661c0
Kendall Jackson <kendallkj00@gmail.com>
2020-06-19 13:53:53 -0400

    Remove comment, last commit add accessibility
    Note: you need vue-click-away

--- CustomSelect.vue
+++ CustomSelect.vue
@@ -1,9 +1,6 @@
 <template>
   <!--
-  All W3 Requirements Met
-  Custom select controls like this require a considerable amount of JS to implement from scratch. We're planning
-  to build some low-level libraries to make this easier with popular frameworks like React, Vue, and even Alpine.js
-  in the near future, but in the mean time we recommend these reference guides when building your implementation:
+  All W3 Requirements Met (Focus trapping)
   https://www.w3.org/TR/wai-aria-practices/#Listbox
   https://www.w3.org/TR/wai-aria-practices/examples/listbox/listbox-collapsible.html
   -->
